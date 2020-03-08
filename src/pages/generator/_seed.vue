92dc61cda2b2b6667407dc8603c66dfc8131913c
lihbr <lihbr@users.noreply.github.com>
2020-03-08 22:08:20 +0100

    feat: add generator random & download

--- src/pages/generator/_seed.vue
+++ src/pages/generator/_seed.vue
@@ -4,6 +4,18 @@
       <canvas ref="canvas" class="canvas" width="1900" height="1150" />
       <label for="seed">Seed:</label>
       <input id="seed" ref="input" v-model="seed" type="text" />
+      <button
+        class="bg-black text-white px-1 opacity-70 hover:opacity-100"
+        @click="seed = '' + Math.random()"
+      >
+        Random
+      </button>
+      <button
+        class="bg-black text-white px-1 opacity-70 hover:opacity-100"
+        @click="download"
+      >
+        Download
+      </button>
     </div>
   </div>
 </template>
@@ -43,6 +55,12 @@ export default {
       const ctx = this.$refs.canvas.getContext("2d");
 
       ctx.putImageData(img, 0, 0);
+    },
+    download() {
+      const link = document.createElement("a");
+      link.download = `generator_${this.seed}.png`;
+      link.href = this.$refs.canvas.toDataURL("image/png");
+      link.click();
     }
   }
 };
